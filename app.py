182f34a6d945c84761bdc08bbf8a3c763b378025
Marlon3010 <164290310+Marlon3010@users.noreply.github.com>
2024-04-04 19:45:36 -0500

    app.py

--- app.py
+++ app.py
@@ -13,7 +13,6 @@ if 'df_experiment_results' not in st.session_state:
 st.header('Lanzar una moneda')
 
 chart = st.line_chart([0.5])
-<<<<<<< HEAD
 
 def toss_coin(n):
 
@@ -51,12 +50,4 @@ if start_button:
     st.session_state['df_experiment_results'] = \\
         st.session_state['df_experiment_results'].reset_index(drop=True)
 
-<<<<<<< HEAD
 st.write(st.session_state['df_experiment_results'])
-=======
-st.write(st.session_state['df_experiment_results'])
-
-Hola
->>>>>>> 37ab4ceb1e3cf96107d0b0d39f3bef0ee55207ba
-=======
->>>>>>> 83315447752a40aa2a7d9cc0bdfee92891f8c254
